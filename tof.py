e3050fd333453222e201ebbd8d43a2140367b707
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-09-08 19:14:35 +0200

    Syntax
    SDA and SCL are on pins 0 and 1. Move to 4 and 5 to free up UART.

--- tof.py
+++ tof.py
@@ -1,16 +1,20 @@
-from machine import Pin,I2C
+from machine import Pin, I2C
 import time
-xshut=Pin(2,Pin.OUT)
+
+xshut = Pin(2, Pin.OUT)
 xshut.off()
 time.sleep_ms(20)
 xshut.on()
 time.sleep_ms(20)
-i2c=I2C(0,sda=Pin(0),scl=Pin(1),freq=400000)
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+#i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
+
 from vl53l0x import VL53L0X
-sensor=VL53L0X(i2c)
+sensor = VL53L0X(i2c)
+
 while True:
     try:
-        mm=sensor.range
+        mm = sensor.range
         print(mm)
     except Exception as e:
         print("err",e)
