6950eed4fb938cf57147409579f074cf178ae005
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-09-09 07:54:15 +0200

    Sensor in continuous mode

--- tof.py
+++ tof.py
@@ -1,22 +1,23 @@
 from machine import Pin, I2C
 import time
+from vl53l0x import VL53L0X
 
-xshut = Pin(2, Pin.OUT)
+SDA_PIN = 0
+SCL_PIN = 1
+XSHUT_PIN = 2
+BUDGET_MS = 1000
+
+xshut = Pin(XSHUT_PIN, Pin.OUT)
 xshut.off()
 time.sleep_ms(20)
 xshut.on()
 time.sleep_ms(20)
-i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
-#i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
-
-from vl53l0x import VL53L0X
+i2c = I2C(0, sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), freq=400000)
 sensor = VL53L0X(i2c)
-
+sensor.measurement_timing_budget = BUDGET_MS * 1000
+sensor.start_continuous()
 while True:
-    try:
-        mm = sensor.range
+    if sensor.data_ready:
+        mm = sensor.read_range()
         print(mm)
-    except Exception as e:
-        print("err",e)
-    time.sleep_ms(200)
 
